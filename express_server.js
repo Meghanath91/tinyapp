d5e959cb3173d67046a78e815fa1e4dd157304a0
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-20 23:59:47 +0000

    POST method added.

--- express_server.js
+++ express_server.js
@@ -41,5 +41,10 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL",(req,res) => {
   let templateVars = {shortURL : req.params.shortURL,longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show",templateVars)
-})
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
