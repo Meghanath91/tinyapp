5e1c5370b409fc2a6f6804cac886b7623337250e
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-21 04:57:25 +0000

    app logic worked.

--- express_server.js
+++ express_server.js
@@ -7,7 +7,6 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -40,24 +39,27 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL",(req,res) => {
   let templateVars = {shortURL : req.params.shortURL,longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show",templateVars)
+  res.render("urls_show",templateVars);
 });
-
-
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+  let longURL = req.body['longURL'];
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+
+  app.get("/u/:shortURL", (req, res) => {
+    res.redirect(longURL);
+  });
+
 });
 
-function generateRandomString() {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < 6; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+const generateRandomString = function() {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
-
-
+};
