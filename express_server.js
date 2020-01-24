8e842f575dc0e7222c1b9993da37981b8ded43dc
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-24 14:59:36 +0000

    Html contents and colors in page updated

--- express_server.js
+++ express_server.js
@@ -1,23 +1,24 @@
+//###Variable initialization#####
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieSession = require('cookie-session');
+const cookieSession = require("cookie-session");// Encrypting Cookies
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");// Encrypting Password
+//Model of Database for Users
 const users = {
-  "userRandomID": {
+  userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
-  "user2RandomID": {
+  user2RandomID: {
     id: "user2RandomID",
     email: "a@a.com",
     password: "123"
   }
 };
-
+//Model of Database for URLs
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
@@ -25,17 +26,15 @@ const urlDatabase = {
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1','keys2'],
-}));
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "keys2"]
+  })
+);
+//sending message to console
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -43,11 +42,7 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+//Main content page
 app.get("/urls", (req, res) => {
   const id = req.session["user_id"];
   const user = users[id];
@@ -55,36 +50,36 @@ app.get("/urls", (req, res) => {
   if (user) {
     let templateVars = {
       urls: urlsForUser(id),
-      email:users[req.session["user_id"]].email
+      email: users[req.session["user_id"]].email
     };
     res.render("urls_index", templateVars);
   } else {
-    res.redirect('/login');
+    res.redirect("/login");
   }
 });
-
+//to show the new urls page
 app.get("/urls/new", (req, res) => {
   let templateVars = {
     user_id: req.session["user_id"],
-    email : users[req.session["user_id"]].email
+    email: users[req.session["user_id"]].email
   };
-  res.render("urls_new",templateVars);
+  res.render("urls_new", templateVars);
 });
 
-app.get("/urls/:shortURL",(req,res) => {
+app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
-    shortURL : req.params.shortURL,
+    shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
     user_id: req.session["user_id"],
     email: users[req.session["user_id"]].email
   };
-  res.render("urls_show",templateVars);
+  res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  let longURL = req.body['longURL'];
-  urlDatabase[shortURL] = { longURL: longURL, userID: req.session["user_id" ]};
+  let longURL = req.body["longURL"];
+  urlDatabase[shortURL] = { longURL: longURL, userID: req.session["user_id"] };
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -92,75 +87,74 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL].longURL);
 });
 
-app.post("/urls/:shortURL/delete",(req,res) => {
+app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect(`/urls`);
 });
 
-app.post("/urls/:shortURL",(req,res) => {
+app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL].longURL = req.body.longURL;
   res.redirect(`/urls`);
 });
-
-app.get("/login",(req,res)=>{
+//#LOGIN page
+app.get("/login", (req, res) => {
   res.render("urls_login");
 });
 
-app.post("/login",(req,res)=>{
+app.post("/login", (req, res) => {
   let user = emailLookUp(req.body.email);
   if (req.body.email.length === 0 || req.body.password === 0) {
     res.send("empty email/password values, Error 400");
-
   } else if (user) {
-    if (passwordChecker(user,req.body.password)) {
+    if (passwordChecker(user, req.body.password)) {
       req.session.user_id = user.id;
-      res.redirect('/urls');
+      res.redirect("/urls");
     } else {
       res.send("invalid password");
     }
   } else {
-    res.send("Please Register");
-  }
+    res.send(`<font size="+4">Please Register</font>`)
+  };
 });
-
-app.post("/logout",(req,res)=>{
+//#LOGOUT page
+app.post("/logout", (req, res) => {
   req.session = null;
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
-
-app.get("/register",(req,res)=>{
+//#REGISTER page
+app.get("/register", (req, res) => {
   res.render("urls_register");
 });
 
-app.post("/register",(req,res) => {
-
+app.post("/register", (req, res) => {
   let randomID = generateRandomString();
   if (req.body.email.length === 0 || req.body.password === 0) {
-    res.send("empty email/password values, Error 400");
+    res.send(`empty email/password values,Error 400`);
   } else if (emailLookUp(req.body.email)) {
     res.send("user already exists, Error 400");
   } else {
     users[randomID] = {
-      id:randomID,
-      email:req.body.email,
-      password:bcrypt.hashSync(req.body.password,10)
+      id: randomID,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10)
     };
     req.session.user_id = randomID;
-    res.redirect('/urls');
-    console.log("this is user",users);
+    res.redirect("/urls");
+    console.log("this is user", users);
   }
 });
-
+// #1 Function to generate Random string
 const generateRandomString = function() {
-  let result           = '';
-  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let charactersLength = characters.length;
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 };
-
+//#2 Function to check email exist
 const emailLookUp = function(emailToCheck) {
   for (let element in users) {
     if (emailToCheck === users[element]["email"]) {
@@ -169,11 +163,11 @@ const emailLookUp = function(emailToCheck) {
   }
   return false;
 };
-
-const passwordChecker = function(user,passwordToCheck) {
-  return bcrypt.compareSync(passwordToCheck,user.password);
+//#3 function to check passwords match
+const passwordChecker = function(user, passwordToCheck) {
+  return bcrypt.compareSync(passwordToCheck, user.password);
 };
-
+//# function to return URLs for particular user/unique id
 const urlsForUser = function(CookieId) {
   let UrlObj = {};
   for (let element in urlDatabase) {
