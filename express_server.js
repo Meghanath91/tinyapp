b3ebcd1f8990505f6fe6397f5fbd6b9e90a587a7
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-24 01:35:05 +0000

    eslinted the code

--- express_server.js
+++ express_server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const cookieSession = require('cookie-session');
-//const cookieParser = require("cookie-parser");
+
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(cookieParser());
 const bcrypt = require('bcrypt');
 
 const users = {
@@ -30,10 +29,7 @@ const urlDatabase = {
 app.use(cookieSession({
   name: 'session',
   keys: ['key1','keys2'],
-
-  // Cookie Options
- // maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -52,18 +48,18 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const id = req.session["user_id"]; //userRandom
+  const id = req.session["user_id"];
   const user = users[id];
 
-  if(user){
-    let templateVars = { 
+  if (user) {
+    let templateVars = {
       urls: urlsForUser(id),
       email:users[req.session["user_id"]].email
     };
     res.render("urls_index", templateVars);
   } else {
-    res.redirect('/login')
-  };
+    res.redirect('/login');
+  }
 });
 
 app.get("/urls/new", (req, res) => {
@@ -81,15 +77,13 @@ app.get("/urls/:shortURL",(req,res) => {
     user_id: req.session["user_id"],
     email: users[req.session["user_id"]].email
   };
-  //console.log(templateVars)
   res.render("urls_show",templateVars);
 });
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   let longURL = req.body['longURL'];
-  urlDatabase[shortURL] = { longURL: longURL, userID: req.session["user_id" ]}
-  
+  urlDatabase[shortURL] = { longURL: longURL, userID: req.session["user_id" ]};
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -98,39 +92,33 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete",(req,res) => {
-  
   delete urlDatabase[req.params.shortURL];
-
   res.redirect(`/urls`);
 });
 
 app.post("/urls/:shortURL",(req,res) => {
-  //console.log(req.params.shortURL)
-   urlDatabase[req.params.shortURL].longURL =req.body.longURL;
-
+  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
   res.redirect(`/urls`);
 });
 
 app.get("/login",(req,res)=>{
-  let templateVars = {users};
   res.render("urls_login");
 });
 
 app.post("/login",(req,res)=>{
-  let user = emailLookUp(req.body.email)
+  let user = emailLookUp(req.body.email);
   if (req.body.email.length === 0 || req.body.password === 0) {
     res.send("empty email/password values, Error 400");
 
   } else if (user) {
-    if(passwordChecker(user,req.body.password)){
-     // res.session("user_id",user.id);
-     req.session.user_id = user.id; 
-     res.redirect('/urls');
+    if (passwordChecker(user,req.body.password)) {
+      req.session.user_id = user.id;
+      res.redirect('/urls');
     } else {
       res.send("invalid password");
     }
   } else {
-    res.send("Please Register")
+    res.send("Please Register");
   }
 });
 
@@ -156,8 +144,7 @@ app.post("/register",(req,res) => {
       email:req.body.email,
       password:bcrypt.hashSync(req.body.password,10)
     };
-    //res.session("user_id",randomID);
-    req.session.user_id = randomID
+    req.session.user_id = randomID;
     res.redirect('/urls');
     console.log("this is user",users);
   }
@@ -183,15 +170,15 @@ const emailLookUp = function(emailToCheck) {
 };
 
 const passwordChecker = function(user,passwordToCheck) {
-  return bcrypt.compareSync(passwordToCheck,user.password)
+  return bcrypt.compareSync(passwordToCheck,user.password);
 };
 
-const urlsForUser = function(CookieId){
-  let UrlObj ={};
-  for(let element in urlDatabase){
-    if(urlDatabase[element].userID === CookieId){
-      UrlObj[element]=urlDatabase[element];
+const urlsForUser = function(CookieId) {
+  let UrlObj = {};
+  for (let element in urlDatabase) {
+    if (urlDatabase[element].userID === CookieId) {
+      UrlObj[element] = urlDatabase[element];
     }
   }
   return UrlObj;
-}
+};
